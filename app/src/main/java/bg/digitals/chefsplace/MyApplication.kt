2c207751e446cd3b36520720544840ff373acbf7
dkutelov <dkutelov@digitals.bg>
2022-11-28 10:54:53 +0200

    Initial commit, App class, dependencies

---
+++ app/src/main/java/bg/digitals/chefsplace/MyApplication.kt
@@ -0,0 +1,8 @@
+package bg.digitals.chefsplace
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+}
---
+++ app/src/main/java/bg/digitals/chefsplace/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package bg.digitals.chefsplace.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bg.digitals.chefsplace.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
